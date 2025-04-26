b62e61cb0b632550b729576ee23f15f13269cde1
Alejandro Diaz <odiaz.dev@gmail.com>
2025-04-26 13:57:10 -0600

    viewChild scroll

--- src/app/gifs/pages/trending-page/trending-page.component.ts
+++ src/app/gifs/pages/trending-page/trending-page.component.ts
@@ -1,28 +1,22 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, ElementRef, inject, signal, viewChild } from '@angular/core';
 import { GifsListComponent } from "../../components/gifs-list/gifs-list.component";
 import { GifsService } from '../../services/gifs.service';
 
-/* const imageUrls: string[] = [
-  "https://flowbite.s3.amazonaws.com/docs/gallery/square/image.jpg",
-  "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-1.jpg",
-  "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-2.jpg",
-  "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-3.jpg",
-  "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-4.jpg",
-  "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-5.jpg",
-  "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-6.jpg",
-  "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-7.jpg",
-  "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-8.jpg",
-  "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-9.jpg",
-  "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-10.jpg",
-  "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-11.jpg"
-];
- */
 @Component({
   selector: 'app-trending-page',
   standalone: true,
-  imports: [GifsListComponent],
+/*   imports: [GifsListComponent], */
   templateUrl: './trending-page.component.html',
 })
-export default class TrendingPageComponent { 
+export default class TrendingPageComponent {
+
   gifService=inject(GifsService);
+
+  scrollDivRef= viewChild<ElementRef>('groupDiv')
+
+  onScroll(event: Event){
+
+    const scrollDiv = this.scrollDivRef()?.nativeElement;
+
+  }
 }
