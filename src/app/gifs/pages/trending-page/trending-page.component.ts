16482f149e29de806b1ccfa89f837a32e87b77a7
Omar Diaz <odiaz.dev@gmail.com>
2025-04-28 14:35:25 -0600

    fin de scroll

--- src/app/gifs/pages/trending-page/trending-page.component.ts
+++ src/app/gifs/pages/trending-page/trending-page.component.ts
@@ -12,11 +12,22 @@ export default class TrendingPageComponent {
 
   gifService=inject(GifsService);
 
-  scrollDivRef= viewChild<ElementRef>('groupDiv')
+  scrollDivRef= viewChild<ElementRef<HTMLDivElement>>('groupDiv')
 
   onScroll(event: Event){
 
     const scrollDiv = this.scrollDivRef()?.nativeElement;
+    if(!scrollDiv)return;
+    
+    const scrollTop = scrollDiv.scrollTop;
+    const clientHeight = scrollDiv.clientHeight;
+    const scrollHeight = scrollDiv.scrollHeight;
+
+    /* console.log({scrollTotal: scrollTop +  clientHeight, scrollHeight}); */
+
+    const isAtBotom = scrollTop+clientHeight+250>=scrollHeight;
+
+    console.log({isAtBotom});
 
   }
 }
